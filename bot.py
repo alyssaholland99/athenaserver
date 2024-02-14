850928a0712c46caecbf0aba55f77e4f1012e340
Alastair Holland (hollan84) <hollan84@coventry.ac.uk>
2024-02-14 23:32:33 +0000

    added all intents

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 
 @client.event
