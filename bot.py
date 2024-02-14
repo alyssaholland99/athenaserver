250ae02de5578d6e8af23d0f29e26ce477a95e2f
Alastair Holland (hollan84) <hollan84@coventry.ac.uk>
2024-02-14 23:54:56 +0000

    more formatting

--- bot.py
+++ bot.py
@@ -33,9 +33,8 @@ async def on_message(message):
                 players = players.splitlines()
                 playerSend = "Players currently online:"
                 for i in players:
-                    if i == "name":
-                        break
-                    playerSend += i.split(",")[0] + "\n"
+                    if i != "name":
+                        playerSend += "\n" + i.split(",")[0]
                 await message.channel.send(playerSend)
 
 client.run(TOKEN)
