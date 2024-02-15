54eb7bbcea5800dce2ba3ccdbbe129e1de6826d6
Alastair Holland (hollan84) <hollan84@coventry.ac.uk>
2024-02-15 00:44:45 +0000

    local ip

--- bot.py
+++ bot.py
@@ -43,7 +43,7 @@ async def on_message(message):
                 restartStatus = os.popen("/srv/dev-disk-by-uuid-8479d8ee-6385-4a78-bdaf-0a485ac3d4c7/palworld/update_restart.sh").read()
                 await message.channel.send(restartStatus)
         if msg.split(" ")[0] == "minecraft":
-            minecraft = JavaServer.lookup("server.alyssaserver.co.uk:25565")
+            minecraft = JavaServer.lookup("192.168.0.120:25565")
             if msg.split(" ")[1] == "players":
                 query = minecraft.query()
                 await message.channel.send(f"The server has the following players online: {', '.join(query.players.names)}")
