211ac9116f9d2266e4271aadd51a19b4141fc881
Alastair Holland (hollan84) <hollan84@coventry.ac.uk>
2024-02-15 00:05:53 +0000

    test2

--- bot.py
+++ bot.py
@@ -40,7 +40,7 @@ async def on_message(message):
                 await message.channel.send(playerSend)
             if msg.split(" ")[1] == "restart":
                 restartStatus = os.popen("/srv/dev-disk-by-uuid-8479d8ee-6385-4a78-bdaf-0a485ac3d4c7/palworld/update_restart.sh").read()
-                if restartStatus != "There are players currently on the world, not restarting":
+                if "restarting" in restartStatus:
                     restartStatus = "The Palworld server is now restarting"
                 await message.channel.send(restartStatus)
 
