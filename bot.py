61e08cde1e864501285d8f864f067e626a1c2a6a
Alastair Holland (hollan84) <hollan84@coventry.ac.uk>
2024-02-25 03:06:46 +0000

    channel fix

--- bot.py
+++ bot.py
@@ -356,8 +356,7 @@ async def on_message(message):
                         if isTrusted(message.author):
                             if len(msg.split(" ")) == 4:
                                 try:
-                                    channel = client.get_channel(int(msg.split(" ")[2]))
-                                    channel = 1207789219540508782
+                                    channel = client.get_channel(1207789219540508782)
                                     messageToDelete = await channel.fetch_message(int(msg.split(" ")[3]))
                                     await messageToDelete.delete()
                                 except Exception as e:
