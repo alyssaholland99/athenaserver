f347e80e036948ee092d706aca9c45fa5be90359
Alastair Holland (hollan84) <hollan84@coventry.ac.uk>
2024-04-02 02:47:55 +0100

    wtf

--- bot.py
+++ bot.py
@@ -222,6 +222,7 @@ async def on_message(message):
                                 for i in currentWhitelist:
                                     await message.channel.send(i["name"])
                             return
+                        await message.channel.send(message.content[1:].split(" ")[2])
                         apiReq = requests.get("https://api.mojang.com/users/profiles/minecraft/" + message.content[1:].split(" ")[2])
                         if ("Couldn't find any profile with name" in str(apiReq)):
                             await message.channel.send("There is no user with this username, please try again")
