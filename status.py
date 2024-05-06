8fbb329b73e9f7a4926451f58e04a325acf64aa1
alyssaholland99 <2011hollanda@gmail.com>
2024-05-06 18:09:40 +0100

    Fixed uptime issue

--- status.py
+++ status.py
@@ -61,6 +61,11 @@ class MyClient(commands.Bot):
                     return
                 await self.offsite_backup_check(channel, urgent, alerts)
 
+            case [18, 10]: #12:00
+                if self.msg_sent:
+                    return
+                await self.offsite_backup_check(channel, urgent, alerts)
+
             case _:
                 self.msg_sent = False
 
@@ -69,13 +74,13 @@ class MyClient(commands.Bot):
         
         date = datetime.datetime.today()
         checkBackup = os.popen('/bin/ssh root@offsitebackup "stat /srv/dev-disk-by-uuid-e6501278-3541-4943-b633-30d3a773bd97/OffsiteBackup"').read()
-        checkBackupUptime = os.popen('/bin/ssh root@offsitebackup "uptime"').read()
+        checkBackupUptime = os.popen('/bin/ssh root@offsitebackup "uptime -p"').read()
         checkBackup = checkBackup.splitlines()
         if len(checkBackup) > 1:
             lastBackup = checkBackup[5].split(" ")[1]
             currentDate = str(date).split(" ")[0]
             if lastBackup == currentDate:
-                await channel.send("SUCCESS: Offsite server was backed up to successfully overnight and has been up for{}".format(checkBackupUptime.split(",")[0].split("up")[1]))
+                await channel.send("SUCCESS: Offsite server was backed up to successfully overnight and has been up for{}".format(checkBackupUptime))
             else:
                 await urgent.send("FAILURE: Backup date and current date do not match; server may not have backed up last night")
         else:
