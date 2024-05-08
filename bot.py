feff9f00d12164312247a83519a4e520992853f3
alyssaholland99 <2011hollanda@gmail.com>
2024-05-08 11:39:47 +0100

    Formatting change

--- bot.py
+++ bot.py
@@ -120,7 +120,7 @@ async def on_message(message):
                         time.sleep(1)
                         power2 = os.popen("cat /sys/class/drm/card0/device/hwmon/hwmon3/energy1_input").read()
                         power3 = int(power2) - int(power1)
-                        await message.channel.send("Current GPU power {} watts".format(round(power3/1000000), 3))
+                        await message.channel.send("Current GPU power: {} watts".format(round(power3/1000000), 3))
                     case _:
                         await message.channel.send(commandError(msg.split(" ")[0]))
 
