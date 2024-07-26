c4d492d445840f9aa2929003cd36cd4bde3aba9f
Alyssa Holland <hollan84@coventry.ac.uk>
2024-07-26 13:43:52 +0100

    added aws uptime

--- bot.py
+++ bot.py
@@ -113,6 +113,10 @@ async def on_message(message):
                             await message.channel.send("Offsite: " + os.popen('/bin/ssh root@offsitebackup "uptime -p"').read())
                         else: 
                             await message.channel.send("Offsite: Offline")
+                        if not "100% packet loss" in os.popen("ping -c 1 uptime.alyssaserver.co.uk").read():
+                            await message.channel.send("AWS: " + os.popen('/bin/ssh -i "/root/aws/us.pem" ec2-user@uptime.alyssaserver.co.uk "uptime -p"').read())
+                        else: 
+                            await message.channel.send("AWS: Offline")
                     case "load":
                         load = (os.popen("/bin/cat /proc/loadavg").read()).split(" ")[0]
                         load = (float(load)/12)*100
