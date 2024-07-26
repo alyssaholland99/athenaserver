4d2fca9d7b20ca6fea235a75d14bfecc706a5718
Alyssa Holland <hollan84@coventry.ac.uk>
2024-07-26 15:11:53 +0100

    Added notification for offline server

---
+++ aws.py
@@ -0,0 +1,36 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+from mcstatus import JavaServer
+from requests import get
+from datetime import datetime
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="for .help"))
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+
+        # don't respond to ourselves
+        if message.author == client.user:
+            return
+
+        if not message.content.startswith('.'):
+            return
+        if message.content.startswith('..'):
+            return
+        
+        if "100% packet loss" in os.popen("ping -c 4 server.alyssaserver.co.uk").read():
+             await message.channel.send("AthenaServer may be offline, please checkout the status page here: <http://uptime.alyssaserver.co.uk/status/main>")
+        
+client.run(TOKEN)
